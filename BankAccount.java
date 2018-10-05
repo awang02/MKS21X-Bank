78f08cfb322844c5d525c0092fc4bdfd8c8f83e4
Allen Wang <awang02@stuy.edu>
2018-10-05 00:33:28 -0400

    Added instance and constructor

---
+++ BankAccount.java
@@ -0,0 +1,15 @@
+// 10/3/18
+public class BankAccount {
+  // instance variables
+  private double balance;
+  private int accountID;
+  private String password;
+
+  // Constructors
+  public BankAccount(double balanceSet, int accountIDSet, String passwordSet) {
+    balance = balanceSet;
+    accountID = accountIDSet;
+    password = passwordSet;
+  }
+
+}
